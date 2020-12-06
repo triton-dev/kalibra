b3bdf1bc50163b8c0d698ab0a97b5810f91d0636
triton-dev <szikoragy@gmail.com>
2020-12-06 21:07:56 +0100

    201206 20:07 install.sql kiegészítése a régi sql függvényekkel.

--- LaTex/sql/install.sql
+++ LaTex/sql/install.sql
@@ -605,7 +605,81 @@ begin
 end;
 $$
 language plpgsql;
+
+-- Alapjelszó beállítása
+--                      fhnev
+-- Function: alapjelszo(text)
+-- DROP FUNCTION alapjelszo(text);
+CREATE OR REPLACE FUNCTION alapjelszo(text)
+	RETURNS boolean AS
+$$
+declare
+	alapjelszo text := md5('init');
+	fh alias for $1;
+begin
+	update felhasznalo set jelszo = alapjelszo where md5(fhnev) = md5(fh);
+	return true;
+end;
+$$
+LANGUAGE plpgsql;
 -------------------------------------------------------------------------------
+
+-- Jelszócsere
+--                      fhnev, uj1, uj2, regi
+-- Function: jelszocsere(text, text, text, text)
+-- DROP FUNCTION jelszocsere(text, text, text, text);
+CREATE OR REPLACE FUNCTION jelszocsere(text, text, text, text)
+	RETURNS boolean AS
+$$
+declare
+	fh alias for $1;
+	uj1 alias for $2;
+	uj2 alias for $3;
+	regi alias for $4;
+begin
+	if uj1 <> uj2 or length(trim(uj1)) = 0 or length(trim(uj2)) = 0
+		or trim(uj1) = trim(regi) or trim(uj2) = trim(regi) then
+			return false;	-- az új jelszavak nem egyeznek meg, vagy üresek,
+							-- vagy nincs változás
+	end if;
+	
+	perform fhnev from felhasznalo where md5(fhnev) = md5(fh) 
+		and jelszo = md5(regi);
+
+	if not found then
+		return false; -- nem található a felhasználónév-jelszó páros
+	else
+		update felhasznalo set jelszo = md5(uj1) where md5(fhnev) = md5(fh);
+		return true; -- sikeres jelszócsere
+	end if;
+end;
+$$
+LANGUAGE plpgsql;
+-------------------------------------------------------------------------------
+
+-- Bejelentkezés
+--                 fhnev, jelszo
+-- Function: login(text, text)
+-- DROP FUNCTION login(text, text);
+CREATE OR REPLACE FUNCTION login(text, text)
+	RETURNS boolean AS
+$$
+declare
+	fh alias for $1;
+	jsz alias for $2;
+begin
+	perform fhnev from felhasznalo where md5(fhnev) = md5(fh)
+		and jelszo = md5(jsz);
+	if found then 
+		return true;
+	end if; -- sikeres bejelentkezés
+	
+	return false;  -- sikertelen bejelentkezés
+end;
+$$
+LANGUAGE plpgsql;
+-------------------------------------------------------------------------------
+
 
 -- Nézetek létrehozása
 
@@ -666,6 +740,8 @@ create view v_dolgozo as
 	union
 	select * from v_passzivdolgozo
 	order by nev;
+
+
 
 
 
