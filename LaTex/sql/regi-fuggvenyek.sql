ce1ee6d342defc5319b7b97912747c31faaefd04
triton-dev <szikoragy@gmail.com>
2020-12-06 20:58:22 +0100

    201206 20:58 régi sql függvények átírása.

--- LaTex/sql/regi-fuggvenyek.sql
+++ LaTex/sql/regi-fuggvenyek.sql
@@ -1,163 +1,66 @@
 -- Function: alapjelszo(text)
-
- 
-
 -- DROP FUNCTION alapjelszo(text);
-
- 
-
 CREATE OR REPLACE FUNCTION alapjelszo(text)
-
-  RETURNS integer AS
-
-$BODY$
-
+	RETURNS boolean AS
+$$
 declare
-
-alapjelszo text := md5('kalibra');
-
+	alapjelszo text := md5('init');
+	fh alias for $1;
 begin
-
-  update felhasznalo set jelszo = alapjelszo where md5(fhnev) = md5($1);
-
-  return 1;
-
+	update felhasznalo set jelszo = alapjelszo where md5(fhnev) = md5(fh);
+	return true;
 end;
-
-$BODY$
-
-  LANGUAGE plpgsql VOLATILE
-
-  COST 100;
-
-ALTER FUNCTION alapjelszo(text)
-
-  OWNER TO gyuri;
-
- 
-
----------------------------------------------------------------------------
-
- 
-
- 
+$$
+LANGUAGE plpgsql;
+-------------------------------------------------------------------------------
 
 -- Function: jelszocsere(text, text, text, text)
-
- 
-
 -- DROP FUNCTION jelszocsere(text, text, text, text);
-
- 
-
-CREATE OR REPLACE FUNCTION jelszocsere(
-
-    text,
-
-    text,
-
-    text,
-
-    text)
-
-  RETURNS integer AS
-
-$BODY$
-
+CREATE OR REPLACE FUNCTION jelszocsere(text, text, text, text)
+	RETURNS boolean AS
+$$
 declare
-
+	fh alias for $1;
+	uj1 alias for $2;
+	uj2 alias for $3;
+	regi alias for $4;
 begin
+	if uj1 <> uj2 or length(trim(uj1)) = 0 or length(trim(uj2)) = 0
+		or trim(uj1) = trim(regi) or trim(uj2) = trim(regi) then
+			return false;	-- az új jelszavak nem egyeznek meg, vagy üresek,
+							-- vagy nincs változás
+	end if;
+	
+	perform fhnev from felhasznalo where md5(fhnev) = md5(fh) 
+		and jelszo = md5(regi);
 
-  if $2 <> $3 or length(trim($2)) = 0 or length(trim($3)) = 0
-
-    or trim($2) = trim($4) or trim($3) = trim($4) then
-
-    return 0; -- az új jelszavak nem egyeznek meg, vagy üresek,
-
-              -- vagy nincs változás
-
-  end if;
-
-  perform fhnev from felhasznalo where md5(fhnev) = md5($1)
-
-    and jelszo = md5($4)
-
-    and meddig is null;
-
-  if not found then
-
-    return 0; -- nem található a felhasználónév-jelszó páros
-
-  else
-
-    update felhasznalo set jelszo = md5($2) where md5(fhnev) = md5($1);
-
-    return 1; -- sikeres jelszócsere
-
-  end if;
-
+	if not found then
+		return false; -- nem található a felhasználónév-jelszó páros
+	else
+		update felhasznalo set jelszo = md5(uj1) where md5(fhnev) = md5(fh);
+		return true; -- sikeres jelszócsere
+	end if;
 end;
-
-$BODY$
-
-  LANGUAGE plpgsql VOLATILE
-
-  COST 100;
-
-ALTER FUNCTION jelszocsere(text, text, text, text)
-
-  OWNER TO gyuri;
-
- 
-
- 
-
---------------------------------------------------------------------------------
-
- 
+$$
+LANGUAGE plpgsql;
+-------------------------------------------------------------------------------
 
 -- Function: login(text, text)
-
- 
-
 -- DROP FUNCTION login(text, text);
-
- 
-
-CREATE OR REPLACE FUNCTION login(
-
-    text,
-
-    text)
-
-  RETURNS integer AS
-
-$BODY$
-
+CREATE OR REPLACE FUNCTION login(text, text)
+	RETURNS boolean AS
+$$
 declare
-
+	fh alias for $1;
+	jsz alias for $2;
 begin
-
-  perform fhnev from felhasznalo where md5(fhnev) = md5($1)
-
-    and jelszo = md5($2)
-
-    and meddig is null;
-
- 
-
-  if found then return 1; end if; -- sikeres bejelentkezés
-
-  return 0;  -- sikertelen bejelentkezés
-
+	perform fhnev from felhasznalo where md5(fhnev) = md5(fh)
+		and jelszo = md5(jsz);
+	if found then 
+		return true;
+	end if; -- sikeres bejelentkezés
+	
+	return false;  -- sikertelen bejelentkezés
 end;
-
-$BODY$
-
-  LANGUAGE plpgsql VOLATILE
-
-  COST 100;
-
-ALTER FUNCTION login(text, text)
-
-  OWNER TO gyuri;
+$$
+LANGUAGE plpgsql;
