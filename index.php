df9ccb628e2e30562445122d5bea969a76d6239e
triton-dev <triton-dev@gmail.com>
2020-12-10 14:44:03 +0100

    201210 14:43 menü bővítése

--- index.php
+++ index.php
@@ -28,6 +28,40 @@ echo"
       <a class='dropdown-item' href='logout/'>Kijelentkezés</a>
     </div>
   </li>
+  <li class='nav-item dropdown'>
+    <a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Dolgozók</a>
+    <div class='dropdown-menu'>
+      <a class='dropdown-item' href='/kalibra/#/'>Aktív dolgozók</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Nem aktív dolgozók</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Minden dolgozó</a>
+      <div class='dropdown-divider'></div>
+      <a class='dropdown-item' href='/kalibra/#/'>Új dolgozó</a>
+    </div>
+  </li>
+    <li class='nav-item dropdown'>
+    <a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Törzsadatok</a>
+    <div class='dropdown-menu'>
+      <a class='dropdown-item' href='/kalibra/#/'>Cikktörzs</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Új cikk</a>
+      <div class='dropdown-divider'></div>
+      <a class='dropdown-item' href='/kalibra/#/'>Költséghelyek</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Új Költséghely</a>
+      <div class='dropdown-divider'></div>
+      <a class='dropdown-item' href='/kalibra/#/'>Partnerek</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Új partner</a>
+      <div class='dropdown-divider'></div>
+      <a class='dropdown-item' href='/kalibra/#/'>Felhasználók</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Új felhasználó</a>
+      <div class='dropdown-divider'></div>
+      <a class='dropdown-item' href='/kalibra/#/'>Szerepek</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Új szerep</a>
+      <div class='dropdown-divider'></div>
+      <a class='dropdown-item' href='/kalibra/#/'>Minősítések</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Eszköztípusok</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Működési módok</a>
+      <a class='dropdown-item' href='/kalibra/#/'>Mértékegységek</a>
+    </div>
+  </li>
   <li class='nav-item'>
     <a class='nav-link' href='#'>Link</a>
   </li>
